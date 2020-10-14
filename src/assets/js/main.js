f532823d842ca0747c1d8b53b6015d1c251fd815
serii <seriiburduja@gmail.com>
2020-10-14 11:29:59 +0300

    ie11

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,10 @@
-import divide from "./modules/libs";
+// import divide from "./modules/libs";
 import animationTitle from "./modules/animation";
 import customJquery from "./modules/custom-jquery";
 window.addEventListener('DOMContentLoaded', function (){
-	console.log(divide(10));
-	console.log(divide(4, 3));
 	animationTitle();
 	customJquery();
+
+	[1,3,4].forEach(item => console.log(item));
+
 });
